c19115a5ff33b19721a743306e827f95282f3467
Kevin Kopp <koppks323@yahoo.com>
2016-07-17 20:04:02 -0600

    what

--- public/modules/compare/config/compare.client.config.js
+++ public/modules/compare/config/compare.client.config.js
@@ -2,7 +2,6 @@
 
 angular.module('compare').run(['Menus',
 	function(Menus) {
-		Menus.addMenuItem('topbar', 'Car Facts Price Comparison', 'compare', '/compare');
 		Menus.addMenuItem('topbar', 'Fuel Savings Calculator', 'cont', '/cont');
 		Menus.addMenuItem('topbar', 'Order CARFAX Report', 'ddd', '/ddd');
 
--- public/modules/compare/config/compare.client.routes.js
+++ public/modules/compare/config/compare.client.routes.js
@@ -3,10 +3,6 @@
 angular.module('compare').config(['$stateProvider',
 	function($stateProvider) {
 		$stateProvider.
-		state('compare', {
-			url: '/compare',
-			templateUrl: 'modules/compare/views/compare-car-client.view.html'
-		}).
 		state('cont', {
 			url: '/cont',
 			templateUrl: 'modules/cont/views/cont-calc.client.view.html'
